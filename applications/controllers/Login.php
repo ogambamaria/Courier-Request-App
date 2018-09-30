8ef18551a48d15c7e3f34a659b6c99bd32cacf05
poppinjay13 <thisian13@gmail.com>
2018-09-30 21:14:27 +0300

    Split
    Auth = Login + Register
    
    Co-Authored-By: ogambamaria <ogambamaria@users.noreply.github.com>

--- applications/controllers/Login.php
+++ applications/controllers/Login.php
@@ -1,5 +1,5 @@
 <?php
-class Auth extends CI_Controller{
+class Login extends CI_Controller{
   public function login(){
     if(isset($_POST['log'])){
       $this->form_validation->set_rules('username','Username','required');
@@ -20,32 +20,9 @@ class Auth extends CI_Controller{
           $this->session->set_flashdata('error','The Account Details you entered do not exist');
           //redirect("Auth/login","refresh");
         }
-
+      }
     }
-  }
     $this->load->view('login');
-}
-  public function register(){
-    if(isset($_POST['register'])){
-      $this->form_validation->set_rules('username','Username','required');
-      $this->form_validation->set_rules('email','Email','required');
-      $this->form_validation->set_rules('password','Password','required|min_length[5]');
-      $this->form_validation->set_rules('password2','Confirm Password','required|min_length[5]|matches[password]');
-      if($this->form_validation->run() == TRUE){
-        $data = array(
-        'username' => $_POST['username'],
-        'email' => $_POST['email'],
-        'password' => $_POST['password'],
-        'gender' => $_POST['gender'],
-        'date' => date('dd/mm/yyyy'),
-        'phone' => $_POST['phone'],
-      );
-      $this->db->insert("users",$data);
-      $this->session->set_flashdata("Success","Account Registered Succesfully. Please login");
-      redirect("Auth/login","refresh");
-    }
-  }
-    $this->load->view('sign_up');
   }
 }
  ?>
