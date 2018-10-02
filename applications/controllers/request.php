4b882048e90fbccdb1fd1bf465c2a5a6b199c61a
poppinjay13 <thisian13@gmail.com>
2018-10-02 10:12:58 +0300

    user-home

--- applications/controllers/request.php
+++ applications/controllers/request.php
@@ -1,26 +1,22 @@
 <?php
 class request extends CI_Controller{
   public function index(){
-    if(isset($_POST['register'])){
-      $this->form_validation->set_rules('username','Username','required');
-      $this->form_validation->set_rules('email','Email','required');
-      $this->form_validation->set_rules('password','Password','required|min_length[5]');
-      $this->form_validation->set_rules('password2','Confirm Password','required|min_length[5]|matches[password]');
+    if(isset($_POST['request'])){
+      $this->form_validation->set_rules('pickup','Pickup location','required');
+      $this->form_validation->set_rules('destination','Destination','required');
       if($this->form_validation->run() == TRUE){
         $data = array(
-        'username' => $_POST['username'],
-        'email' => $_POST['email'],
-        'password' => $_POST['password'],
-        'gender' => $_POST['gender'],
-        'date' => date('dd/mm/yyyy'),
-        'phone' => $_POST['phone'],
+        'username' => 'Ian',//$_SESSION['username'],
+        'pickup' => $_POST['pickup'],
+        'destination' => $_POST['destination'],
+        'rider_type' => $_POST['rider_type']
       );
-      $this->db->insert("users",$data);
-      $this->session->set_flashdata("Success","Account Registered Succesfully. Please login");
-      redirect("Login","refresh");
+      $this->db->insert("user_requests",$data);
+      //$this->session->set_flashdata("Success","Request succesfully recieved");
+      redirect("request","refresh");
     }
   }
-    $this->load->view('register');
-  }
+    $this->load->view('user-home');
   }
 }
+?>
