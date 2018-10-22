0b87476fb9aaebc4590554765a39bcd550fdba4f
Tiffany Tirop <32842214+tiffytirop@users.noreply.github.com>
2018-10-22 21:43:17 +0300

    Add files via upload
    idk whats up with the map

---
+++ applications/views/rider-homepage.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Home | Tumia</title>
+        <link rel="icon" href="project/images/icons8_Stork_With_Bundle_96px_1.png">
+
+        <style>
+            body {
+                font-family: sans-serif;
+            }
+
+            ul {
+                list-style-type: none;
+                margin: 0;
+                padding: 5px 10px;
+                overflow: hidden;
+            }
+
+            li {
+                float: right;
+                border: 2px black;
+            }
+
+            li a, .dropbtn {
+                display: block;
+                color: black;
+                text-align: center;
+                padding: 5px 15px;
+                text-decoration: none;
+            }
+
+            li a:hover, .dropdown:hover .dropbtn {
+                background-color: skyblue;
+            }
+
+            li.dropdown {
+                display: inline-block;
+                color: black;
+                font-size: 16px;
+            }
+
+            .dropdown-content {
+                display: none;
+                position: absolute;
+                background-color: #f9f9f9;
+                min-width: 160px;
+                box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+                z-index: 1;
+            }
+
+            .dropdown-content a {
+                color: black;
+                padding: 12px 16px;
+                text-decoration: none;
+                display: block;
+                text-align: left;
+            }
+
+            .dropdown-content a:hover {background-color: skyblue}
+
+            .dropdown:hover .dropdown-content {
+                display: block;
+            }
+
+            .googleMap {
+                position: absolute;
+            }
+
+      
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+}
+
+.switch input { 
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 26px;
+  width: 26px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+input:checked + .slider {
+  background-color: #2196F3;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #2196F3;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(26px);
+  -ms-transform: translateX(26px);
+  transform: translateX(26px);
+}
+
+/* Rounded sliders */
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+}
+
+           
+
+        </style>
+
+    </head>
+
+    <body>
+        <div class="nav-bar">
+            <ul>
+                <li><a href="#"><p>HI @Username</p></a></li>
+                <li class="dropdown">
+                    <a href="javascript:void(0)" class="dropbtn">MENU</a>
+                    <div class="dropdown-content">
+                        <a href="#">Account Details</a>
+                        <a href="#"> History</a>
+                        <a href="#">waybill</a>
+                        <a href="#">Earnings</a>
+                        <a href="#">Weekly Ratings Report</a>
+                        <a href="#">Documents</a>
+                        <a href="#">Invites</a>
+
+
+
+
+                    </div>
+                </li>
+            </ul>
+        </div>
+
+
+
+<label class="switch">
+  <input type="checkbox" checked>
+  <span class="slider round"></span>
+</label>
+ 
+        <div class="container">
+            <div id="googleMap" style="width:100%; height: 750px;"></div>
+
+            
+        
+ 
+
+        </div>
+
+        <script>
+            function myMap() {
+            var mapProp= {
+                center:new google.maps.LatLng(-1.28333 , 36.81667),
+                zoom:12,
+            };
+            var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+            }
+        </script>
+
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAcM_n70Tmi9V87FRnw0AesYjqz2qUVdbI&libraries=places&callback=initMap"></script>
+
+
+
+    </body>
+
+</html>
